884943a54e81a5bee93da26d5f08eb5f90f89211
BIBOLV <49994376+BIBOLV@users.noreply.github.com>
2019-04-25 23:32:28 +0300

    Update uninstall.sh
    Add missing removals of installed files and directories

--- uninstall.sh
+++ uninstall.sh
@@ -14,12 +14,30 @@ then
     echo binaries: removed $ROOT/usr/bin/wmbusmeters and $ROOT/usr/sbin/wmbusmetersd
 fi
 
+if [ -f $ROOT/usr/share/man/man1/wmbusmeters.1.gz ]
+then
+    rm $ROOT/usr/share/man/man1/wmbusmeters.1.gz
+    echo man page: removed $ROOT/usr/share/man/man1/wmbusmeters.1.gz
+fi
+
 ID=$(id -u wmbusmeters 2>/dev/null)
 
 if [ ! "$ID" = "" ]
 then
     deluser wmbusmeters
     echo user: removed wmbusmeters
+fi
+
+if [ -d $ROOT/var/log/wmbusmeters/ ]
+then
+    rm -rf $ROOT/var/log/wmbusmeters/
+    echo log dir: removed $ROOT/var/log/wmbusmeters/
+fi
+
+if [ -f $ROOT/etc/logrotate.d/wmbusmeters ]
+then
+    rm $ROOT/etc/logrotate.d/wmbusmeters
+    echo logrotate file: removed $ROOT/etc/logrotate.d/wmbusmeters
 fi
 
 if [ -f $ROOT/etc/wmbusmeters.conf ]
@@ -38,4 +56,10 @@ if [ -f $ROOT/etc/systemd/system/wmbusmeters.service ]
 then
     rm $ROOT/etc/systemd/system/wmbusmeters.service
     echo systemd: removed $ROOT/etc/systemd/system/wmbusmeters.service
+fi
+
+if [ -f $ROOT/etc/udev/rules.d/99-wmbus-usb-serial.rules ]
+then
+    rm $ROOT/etc/udev/rules.d/99-wmbus-usb-serial.rules
+    echo udev: removed $ROOT/etc/udev/rules.d/99-wmbus-usb-serial.rules
 fi
