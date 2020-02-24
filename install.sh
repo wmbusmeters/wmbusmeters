d555f0649f42a7ca5d055abad668260848614a6c
Jacek Tomasiak <jacek.tomasiak@gmail.com>
2020-02-24 22:31:45 +0100

    Fix wmbusmetersd symlink
    When building the package, install script creates incorrect symlink
    pointing to the build directory instead of target root.

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ rm -f "$ROOT"/usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd
 mkdir -p "$ROOT"/usr/bin
 mkdir -p "$ROOT"/usr/sbin
 cp "$SRC" "$ROOT"/usr/bin/wmbusmeters
-ln -s "$ROOT"/usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd
+ln -s /usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd
 
 echo binaries: installed "$ROOT"/usr/bin/wmbusmeters and "$ROOT"/usr/sbin/wmbusmetersd
 
