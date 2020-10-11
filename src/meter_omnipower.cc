fd5c4e171f95ad66d95962e651b811d029126c38
Janus Bo Andersen <janus@janusboandersen.dk>
2020-10-11 20:53:52 +0800

    Add additional fields to OmniPower for testing and allow debug printout

--- src/meter_omnipower.cc
+++ src/meter_omnipower.cc
@@ -21,17 +21,27 @@
 #include"wmbus.h"
 #include"wmbus_utils.h"
 #include"util.h"
+#include <iostream>
 
 struct MeterOmnipower : public virtual ElectricityMeter, public virtual MeterCommonImplementation {
     MeterOmnipower(WMBus *bus, MeterInfo &mi);
 
     double totalEnergyConsumption(Unit u);
 
+    double unknownField1(Unit u);
+    double unknownField2(Unit u);
+    double unknownField3(Unit u);
+    double unknownField4(Unit u);
+
 private:
 
     void processContent(Telegram *t);
 
-    double total_energy_kwh_ {};
+    double total_energy_kwh_{};
+    double field_1_ {};
+    double field_2_ {};
+    double field_3_ {};
+    double field_4_ {};
 };
 
 unique_ptr<ElectricityMeter> createOmnipower(WMBus *bus, MeterInfo &mi)
@@ -58,6 +68,31 @@ double MeterOmnipower::totalEnergyConsumption(Unit u)
     return convert(total_energy_kwh_, Unit::KWH, u);
 }
 
+double MeterOmnipower::unknownField1(Unit u)
+{
+    assertQuantity(u, Quantity::Energy);
+    return convert(field_1_, Unit::KWH, u);
+}
+
+double MeterOmnipower::unknownField2(Unit u)
+{
+    assertQuantity(u, Quantity::Energy);
+    return convert(field_2_, Unit::KWH, u);
+}
+
+double MeterOmnipower::unknownField3(Unit u)
+{
+    assertQuantity(u, Quantity::Energy);
+    return convert(field_3_, Unit::KWH, u);
+}
+
+double MeterOmnipower::unknownField4(Unit u)
+{
+    assertQuantity(u, Quantity::Energy);
+    return convert(field_4_, Unit::KWH, u);
+}
+
+
 void MeterOmnipower::processContent(Telegram *t)
 {
     // Meter record:
@@ -69,4 +104,6 @@ void MeterOmnipower::processContent(Telegram *t)
     int offset;
     extractDVdouble(&t->values, "04833B", &offset, &total_energy_kwh_);
     t->addMoreExplanation(offset, " total power (%f kwh)", total_energy_kwh_);
+
+    std::cout << "Offset = " << offset << std::endl;
 }
