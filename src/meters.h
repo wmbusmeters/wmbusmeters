f780f74384713750b1702edd0c1caa03fa2f4a58
Fredrik Öhrström <oehrstroem@gmail.com>
2021-01-24 20:36:59 +0100

    Allow T1 mode for multical302

--- src/meters.h
+++ src/meters.h
@@ -56,7 +56,7 @@
     X(mkradio3,   T1_bit, WaterMeter,       MKRADIO3,    MKRadio3)     \
     X(mkradio4,   T1_bit, WaterMeter,       MKRADIO4,    MKRadio4)     \
     X(multical21, C1_bit|T1_bit, WaterMeter,       MULTICAL21,  Multical21)   \
-    X(multical302,C1_bit, HeatMeter,        MULTICAL302, Multical302)  \
+    X(multical302,C1_bit|T1_bit, HeatMeter,        MULTICAL302, Multical302)  \
     X(multical403,C1_bit, HeatMeter,        MULTICAL403, Multical403)  \
     X(multical603,C1_bit, HeatMeter,        MULTICAL603, Multical603)  \
     X(multical803,C1_bit, HeatMeter,        MULTICAL803, Multical803)  \
