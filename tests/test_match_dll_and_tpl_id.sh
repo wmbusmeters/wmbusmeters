39f553322a41ca37990af64b6b1c0d9e02798b01
Fredrik Öhrström <oehrstroem@gmail.com>
2021-01-30 18:17:25 +0100

    Add test files.

---
+++ tests/test_match_dll_and_tpl_id.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+PROG="$1"
+
+mkdir -p testoutput
+
+TEST=testoutput
+
+TESTNAME="Test match on dll id"
+TESTRESULT="ERROR"
+
+cat simulations/simulation_dll_tpl_diff.txt | grep '^{' > $TEST/test_expected.txt
+$PROG --format=json simulations/simulation_dll_tpl_diff.txt \
+      Vatten hydrus 81818181 NOKEY \
+      > $TEST/test_output.txt 2> $TEST/test_stderr.txt
+
+if [ "$?" = "0" ]
+then
+    cat $TEST/test_output.txt | sed 's/"timestamp":"....-..-..T..:..:..Z"/"timestamp":"1111-11-11T11:11:11Z"/' > $TEST/test_responses.txt
+    diff $TEST/test_expected.txt $TEST/test_responses.txt
+    if [ "$?" = "0" ]
+    then
+        echo OK: $TESTNAME
+        TESTRESULT="OK"
+    fi
+else
+    echo "wmbusmeters returned error code: $?"
+    cat $TEST/test_output.txt
+    cat $TEST/test_stderr.txt
+fi
+
+if [ "$TESTRESULT" = "ERROR" ]
+then
+    echo ERROR: $TESTNAME
+    exit 1
+fi
+
+
+TESTNAME="Test match on tpl id"
+TESTRESULT="ERROR"
+
+cat simulations/simulation_dll_tpl_diff.txt | grep '^{' > $TEST/test_expected.txt
+$PROG --format=json simulations/simulation_dll_tpl_diff.txt \
+      Vatten hydrus 56465646 NOKEY \
+      > $TEST/test_output.txt 2> $TEST/test_stderr.txt
+
+if [ "$?" = "0" ]
+then
+    cat $TEST/test_output.txt | sed 's/"timestamp":"....-..-..T..:..:..Z"/"timestamp":"1111-11-11T11:11:11Z"/' > $TEST/test_responses.txt
+    diff $TEST/test_expected.txt $TEST/test_responses.txt
+    if [ "$?" = "0" ]
+    then
+        echo OK: $TESTNAME
+        TESTRESULT="OK"
+    fi
+else
+    echo "wmbusmeters returned error code: $?"
+    cat $TEST/test_output.txt
+    cat $TEST/test_stderr.txt
+fi
+
+if [ "$TESTRESULT" = "ERROR" ]
+then
+    echo ERROR: $TESTNAME
+    exit 1
+fi
