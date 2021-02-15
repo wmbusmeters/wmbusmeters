3e28dbc17f33180b6bfb5512cc262a1640f6f14a
Don-vip <vincent.privat@gmail.com>
2021-02-15 02:21:15 +0100

    Fix IZAR manufacture year parsing

--- tests/test_izars.sh
+++ tests/test_izars.sh
@@ -16,7 +16,8 @@ rm -f $LOGFILE
 METERS="IzarWater   izar        21242472 NOKEY
         IzarWater2  izar        66236629 NOKEY
         IzarWater3  izar        20481979 NOKEY
-        IzarWater4  izar        2124589c NOKEY"
+        IzarWater4  izar        2124589c NOKEY
+        IzarWater5  izar        20e4ffde NOKEY"
 
 cat simulations/simulation_izars.txt | grep '^{' > $TEST/test_expected.txt
 $PROG --format=json simulations/simulation_izars.txt $METERS > $TEST/test_output.txt 2> $TEST/test_stderr.txt
@@ -64,6 +65,11 @@ Received telegram from: 20481979
                    ver: 0x78
                 driver: izar
 Received telegram from: 2124589c
+          manufacturer: (SAP) Sappel (0x4c30)
+                  type: Water meter (0x07)
+                   ver: 0x00
+                driver: izar
+Received telegram from: 20e4ffde
           manufacturer: (SAP) Sappel (0x4c30)
                   type: Water meter (0x07)
                    ver: 0x00
