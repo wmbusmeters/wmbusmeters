e0488aa68e5f90486c52d258b41fcce8a842a6ce
Tomasz Gramza <tgramza@olx.pl>
2021-08-26 18:46:00 +0000

    Allow to use custom configuration for MQTT

--- ha-addon/mosquitto_pub.sh
+++ ha-addon/mosquitto_pub.sh
@@ -3,8 +3,13 @@
 TOPIC=$1
 MESSAGE=$2
 
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
+MQTT_HOST=$(bashio::config "mqtt.host")
+if [ -z "$MQTT_HOST"]; then MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_PORT=$(bashio::config "mqtt.port")
+if [ -z "$MQTT_HOST"]; then MQTT_PORT=$(bashio::services mqtt "port")
+MQTT_USER=$(bashio::config "mqtt.username")
+if [ -z "$MQTT_USER"]; then MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::config "mqtt.password")
+if [ -z "$MQTT_PASSWORD"]; then MQTT_PASSWORD=$(bashio::services mqtt "password")
 
-/usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASSWORD -t $TOPIC -m "$MESSAGE"
+/usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -t $TOPIC -m "$MESSAGE"
