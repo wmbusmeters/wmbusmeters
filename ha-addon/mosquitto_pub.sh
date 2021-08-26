cca3232beb39ae31e9ab408074876df79b16b860
Tomasz Gramza <tgramza@olx.pl>
2021-08-26 20:34:47 +0000

    Fix mosquitto_pub.sh

--- ha-addon/mosquitto_pub.sh
+++ ha-addon/mosquitto_pub.sh
@@ -4,12 +4,12 @@ TOPIC=$1
 MESSAGE=$2
 
 MQTT_HOST=$(bashio::config "mqtt.host")
-if [ -z "$MQTT_HOST"]; then MQTT_HOST=$(bashio::services mqtt "host"); fi
+if [[] -z "$MQTT_HOST" ]]; then MQTT_HOST=$(bashio::services mqtt "host"); fi
 MQTT_PORT=$(bashio::config "mqtt.port")
-if [ -z "$MQTT_PORT"]; then MQTT_PORT=$(bashio::services mqtt "port"); fi
+if [[ -z "$MQTT_PORT" ]]; then MQTT_PORT=$(bashio::services mqtt "port"); fi
 MQTT_USER=$(bashio::config "mqtt.username")
-if [ -z "$MQTT_USER"]; then MQTT_USER=$(bashio::services mqtt "username"); fi
+if [[ -z "$MQTT_USER" ]]; then MQTT_USER=$(bashio::services mqtt "username"); fi
 MQTT_PASSWORD=$(bashio::config "mqtt.password")
-if [ -z "$MQTT_PASSWORD"]; then MQTT_PASSWORD=$(bashio::services mqtt "password"); fi
+if [[ -z "$MQTT_PASSWORD" ]]; then MQTT_PASSWORD=$(bashio::services mqtt "password"); fi
 
 /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASSWORD -t $TOPIC -m "$MESSAGE"
