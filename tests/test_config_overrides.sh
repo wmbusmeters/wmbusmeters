29c7177e8c8b303374f6da7b7c67474e715de726
Fredrik Öhrström <oehrstroem@gmail.com>
2022-02-05 16:09:39 +0100

    Update test to run on build server.

--- tests/test_config_overrides.sh
+++ tests/test_config_overrides.sh
@@ -10,8 +10,12 @@ mkdir -p $TEST
 TESTNAME="Test config override with oneshot"
 TESTRESULT="ERROR"
 
+BROKEN="T1;1;1;2019-04-03 19:00:42.000;97;148;18888888;0x6e4401068888881805077a85006085bc2630713819512eb4cd87fba554fb43f67cf9654a68ee8e194088160df752e716238292e8af1ac20986202ee561d743602466915e42f1105d9c6782"
+
 cat simulations/serial_aes.msg | grep '^{' | tr -d '#' > $TEST/test_expected.txt
 cat simulations/serial_aes.msg | grep '^[CT]' | tr -d '#' > $TEST/test_input.txt
+echo "$BROKEN" >> $TEST/input.txt
+
 cat $TEST/test_input.txt | $PROG --useconfig=tests/config9 --device=stdin:rtlwmbus --oneshot > $TEST/test_output.txt 2> $TEST/test_stderr.txt
 
 if ! grep -q "(main) all meters have received at least one update, stopping." $TEST/test_stderr.txt
