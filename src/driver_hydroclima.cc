3282c1934415d8fe039756cb8b51d2e6565f0307
Fredrik Öhrström <oehrstroem@gmail.com>
2022-05-05 21:16:03 +0200

    Handle both types of hydroclima telegrams.

--- src/driver_hydroclima.cc
+++ src/driver_hydroclima.cc
@@ -23,6 +23,8 @@ namespace
     {
         Driver(MeterInfo &mi, DriverInfo &di);
         void processContent(Telegram *t);
+        void decodeRF_RKN0(Telegram *t);
+        void decodeRF_RKN9(Telegram *t);
 
         double average_ambient_temperature_ {};
         double max_ambient_temperature_ {};
@@ -75,7 +77,6 @@ namespace
                  GET_FUNC(average_heater_temperature_last_month_, Unit::C),
                  "Average heater temperature last month.",
                  PrintProperty::JSON);
-
     }
 
     double toTemperature(uchar hi, uchar lo)
@@ -98,6 +99,18 @@ namespace
     {
         if (t->mfct_0f_index == -1) return; // Check that there is mfct data.
 
+        if (t->dv_entries.count("036E") != 0)
+        {
+            decodeRF_RKN0(t);
+        }
+        else
+        {
+            decodeRF_RKN9(t);
+        }
+    }
+
+    void Driver::decodeRF_RKN0(Telegram *t)
+    {
         int offset = t->header_size+t->mfct_0f_index;
 
         vector<uchar> bytes;
@@ -108,8 +121,6 @@ namespace
         int i = 0;
         int len = bytes.size();
         string info;
-
-        // Typicall 25 bytes: 100043106A7D2C4A078F12202CB1242A06D306210021000000
 
         if (i >= len) return;
         uchar frame_identifier = bytes[i];
@@ -188,6 +199,45 @@ namespace
                                  "*** %02X%02X%02X total indication u %f", bytes[i], bytes[i+1], bytes[i+2], total_indication_u);
         i+=3;
     }
+
+    void Driver::decodeRF_RKN9(Telegram *t)
+    {
+        int offset = t->header_size+t->mfct_0f_index;
+
+        vector<uchar> bytes;
+        t->extractMfctData(&bytes); // Extract raw frame data after the DIF 0x0F.
+
+        debugPayload("(hydroclima mfct)", bytes);
+
+        int i = 0;
+        int len = bytes.size();
+        string info;
+
+        double last_x_month_uc {};
+        for (int m = 1; m <= 12; ++m)
+        {
+            if (i+1 >= len) return;
+            last_x_month_uc = toIndicationU(bytes[i+1], bytes[i]);
+            t->addSpecialExplanation(i+offset, 2, KindOfData::CONTENT, Understanding::FULL,
+                                     "*** %02X%02X last %d month uc %f", bytes[i], bytes[i+1], m, last_x_month_uc);
+            i+=2;
+        }
+
+        if (i+1 >= len) return;
+        uint16_t date_case_opened = bytes[i+1]<<8 | bytes[i];
+        t->addSpecialExplanation(i+offset, 2, KindOfData::CONTENT, Understanding::FULL,
+                                 "*** %02X%02X date case opened %x", bytes[i], bytes[i+1], date_case_opened);
+        i+=2;
+
+        if (i+1 >= len) return;
+        uint16_t date_start_month = bytes[i+1]<<8 | bytes[i];
+        t->addSpecialExplanation(i+offset, 2, KindOfData::CONTENT, Understanding::FULL,
+                                 "*** %02X%02X date start month %x", bytes[i], bytes[i+1], date_start_month);
+        i+=2;
+
+        // The test telegram I have has more data, but the specification I have ends here!?!
+    }
+
 }
 
 
@@ -196,3 +246,10 @@ namespace
 // telegram=|2e44b0099861036853087a000020002F2F036E0000000F100043106A7D2C4A078F12202CB1242A06D3062100210000|
 // {"media":"heat cost allocation","meter":"hydroclima","name":"HCA","id":"68036198","current_consumption_hca":0,"average_ambient_temperature_c":18.66,"max_ambient_temperature_c":47.51,"average_ambient_temperature_last_month_c":15.78,"average_heater_temperature_last_month_c":17.47,"timestamp":"1111-11-11T11:11:11Z"}
 // |HCA;68036198;0.000000;18.660000;1111-11-11 11:11.11
+
+
+// Test: HCAA hydroclima 74393723 NOKEY
+// Comment:
+// telegram=|2D44B009233739743308780F9D1300023ED97AEC7BC5908A32C15D8A32C126915AC15AC126912691269187912689|
+// {"media":"heat cost allocation","meter":"hydroclima","name":"HCAA","id":"74393723","current_consumption_hca":null,"average_ambient_temperature_c":0,"max_ambient_temperature_c":0,"average_ambient_temperature_last_month_c":0,"average_heater_temperature_last_month_c":0,"timestamp":"1111-11-11T11:11:11Z"}
+// |HCAA;74393723;nan;0.000000;1111-11-11 11:11.11
