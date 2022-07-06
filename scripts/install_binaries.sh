b545100c97523e7602c9fcc20f3695745803c0e9
Fredrik Öhrström <oehrstroem@gmail.com>
2022-07-06 09:53:22 +0200

    Install wmbusmeters-admin.

--- scripts/install_binaries.sh
+++ scripts/install_binaries.sh
@@ -1,9 +1,11 @@
-# Copyright (C) 2021 Fredrik Öhrström (gpl-3.0-or-later)
+# Copyright (C) 2021-2022 Fredrik Öhrström (gpl-3.0-or-later)
 
-rm -f "$ROOT"/usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd
+rm -f "$ROOT"/usr/bin/wmbusmeters "$ROOT"/usr/sbin/wmbusmetersd "$ROOT"/usr/bin/wmbusmeters-admin
 mkdir -p "$ROOT"/usr/bin
 mkdir -p "$ROOT"/usr/sbin
 cp "$SRC" "$ROOT"/usr/bin/wmbusmeters
+cp "${SRC}-admin" "$ROOT"/usr/bin/wmbusmeters-admin
+
 (cd "$ROOT"/usr/sbin; ln -s ../bin/wmbusmeters wmbusmetersd)
 
-echo "binaries: installed $ROOT/usr/bin/wmbusmeters and $ROOT/usr/sbin/wmbusmetersd"
+echo "binaries: installed $ROOT/usr/bin/wmbusmeters $ROOT/usr/sbin/wmbusmetersd $ROOT/usr/bin/wmbusmeters-admin"
