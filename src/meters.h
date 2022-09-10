5c5813ef8d73981f1eb5c1a1cadd203df5a2e403
Fredrik Öhrström <oehrstroem@gmail.com>
2022-09-10 20:01:52 +0200

    Refactor driver weh_07.

--- src/meters.h
+++ src/meters.h
@@ -93,7 +93,6 @@ LIST_OF_METER_TYPES
     X(topaseskr,  T1_bit, WaterMeter,   TOPASESKR, TopasEsKr)          \
     X(vario451,   T1_bit, HeatMeter,        VARIO451,    Vario451)     \
     X(lse_08,     S1_bit|C1_bit, HeatCostAllocationMeter, LSE_08, LSE_08) \
-    X(weh_07,     C1_bit, WaterMeter,       WEH_07,       WEH_07)      \
     X(unismart,   T1_bit, GasMeter,       UNISMART, Unismart)  \
 
 
