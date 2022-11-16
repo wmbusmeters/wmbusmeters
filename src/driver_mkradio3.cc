08fae1b1a6b749d25862a8d149a76dd5db5fa932
Fredrik Öhrström <oehrstroem@gmail.com>
2022-11-16 16:14:04 +0100

    Refactor mkradio3 to new driver format.

---
+++ src/driver_mkradio3.cc
@@ -0,0 +1,178 @@
+/*
+ Copyright (C) 2019-2022 Fredrik Öhrström (gpl-3.0-or-later)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include"meters_common_implementation.h"
+
+namespace
+{
+    struct Driver : public virtual MeterCommonImplementation
+    {
+        Driver(MeterInfo &mi, DriverInfo &di);
+
+        void processContent(Telegram *t);
+
+    private:
+
+        double totalWaterConsumption(Unit u);
+        bool  hasTotalWaterConsumption();
+        double targetWaterConsumption(Unit u);
+        bool  hasTargetWaterConsumption();
+        string currentDate();
+        string previousDate();
+
+        double total_water_consumption_m3_ {};
+        double target_water_consumption_m3_ {};
+        string current_date_ {};
+        string previous_date_ {};
+    };
+
+    static bool ok = registerDriver([](DriverInfo&di)
+    {
+        di.setName("mkradio3");
+        di.setMeterType(MeterType::WaterMeter);
+        di.addLinkMode(LinkMode::T1);
+        di.addDetection(MANUFACTURER_TCH, 0x62,  0x74);
+        di.addDetection(MANUFACTURER_TCH, 0x72,  0x74);
+
+        di.setConstructor([](MeterInfo& mi, DriverInfo& di){ return shared_ptr<Meter>(new Driver(mi, di)); });
+    });
+
+    Driver::Driver(MeterInfo &mi, DriverInfo &di) : MeterCommonImplementation(mi, di)
+    {
+        addPrint("total", Quantity::Volume,
+                 [&](Unit u){ return totalWaterConsumption(u); },
+                 "The total water consumption recorded by this meter.",
+                 PrintProperty::FIELD | PrintProperty::JSON);
+
+        addPrint("target", Quantity::Volume,
+                 [&](Unit u){ return targetWaterConsumption(u); },
+                 "The total water consumption recorded at the beginning of this month.",
+                 PrintProperty::FIELD | PrintProperty::JSON);
+
+        addPrint("current_date", Quantity::Text,
+                 [&](){ return currentDate(); },
+                 "Date of current billing period.",
+                 PrintProperty::FIELD | PrintProperty::JSON);
+
+        addPrint("prev_date", Quantity::Text,
+                 [&](){ return previousDate(); },
+                 "Date of previous billing period.",
+                 PrintProperty::FIELD | PrintProperty::JSON);
+    }
+
+    void Driver::processContent(Telegram *t)
+    {
+        // Unfortunately, the MK Radio 3 is mostly a proprieatary protocol
+        // simple wrapped inside a wmbus telegram since the ci-field is 0xa2.
+        // Which means that the entire payload is manufacturer specific.
+
+        map<string,pair<int,DVEntry>> vendor_values;
+        vector<uchar> content;
+
+        t->extractPayload(&content);
+
+        // Previous date
+        uint16_t prev_date = (content[2] << 8) | content[1];
+        uint prev_date_day = (prev_date >> 0) & 0x1F;
+        uint prev_date_month = (prev_date >> 5) & 0x0F;
+        uint prev_date_year = (prev_date >> 9) & 0x3F;
+        strprintf(&previous_date_, "%d-%02d-%02dT02:00:00Z", prev_date_year+2000, prev_date_month, prev_date_day);
+
+        string prev_date_str;
+        strprintf(&prev_date_str, "%04x", prev_date);
+        uint offset = t->parsed.size() + 1;
+        vendor_values["0215"] = { offset, DVEntry(offset, DifVifKey("0215"), MeasurementType::Instantaneous, 0x6c, {}, 0, 0, 0, prev_date_str) };
+        t->explanations.push_back(Explanation(offset, 1, prev_date_str, KindOfData::CONTENT, Understanding::FULL));
+        t->addMoreExplanation(offset, " previous date (%s)", previous_date_.c_str());
+
+        // Previous consumption
+        uchar prev_lo = content[3];
+        uchar prev_hi = content[4];
+        double prev = (256.0*prev_hi+prev_lo)/10.0;
+
+        string prevs;
+        strprintf(&prevs, "%02x%02x", prev_lo, prev_hi);
+        offset = t->parsed.size()+3;
+        vendor_values["0215"] = { offset, DVEntry(offset, DifVifKey("0215"), MeasurementType::Instantaneous, 0x15, {}, 0, 0, 0, prevs) };
+        t->explanations.push_back(Explanation(offset, 2, prevs, KindOfData::CONTENT, Understanding::FULL));
+        t->addMoreExplanation(offset, " prev consumption (%f m3)", prev);
+
+        // Current date
+        uint16_t current_date = (content[6] << 8) | content[5];
+        uint current_date_day = (current_date >> 4) & 0x1F;
+        uint current_date_month = (current_date >> 9) & 0x0F;
+        strprintf(&current_date_, "%s-%02d-%02dT02:00:00Z", currentYear().c_str(), current_date_month, current_date_day);
+
+        string current_date_str;
+        strprintf(&current_date_str, "%04x", current_date);
+        offset = t->parsed.size() + 5;
+        vendor_values["0215"] = { offset, DVEntry(offset, DifVifKey("0215"), MeasurementType::Instantaneous, 0x6c, {}, 0, 0, 0, current_date_str) };
+        t->explanations.push_back(Explanation(offset, 1, current_date_str, KindOfData::CONTENT, Understanding::FULL));
+        t->addMoreExplanation(offset, " current date (%s)", current_date_.c_str());
+
+        // Current consumption
+        uchar curr_lo = content[7];
+        uchar curr_hi = content[8];
+        double curr = (256.0*curr_hi+curr_lo)/10.0;
+
+        string currs;
+        strprintf(&currs, "%02x%02x", curr_lo, curr_hi);
+        offset = t->parsed.size()+7;
+        vendor_values["0215"] = { offset, DVEntry(offset, DifVifKey("0215"), MeasurementType::Instantaneous, 0x15, {}, 0, 0, 0, currs) };
+        t->explanations.push_back(Explanation(offset, 2, currs, KindOfData::CONTENT, Understanding::FULL));
+        t->addMoreExplanation(offset, " curr consumption (%f m3)", curr);
+
+        total_water_consumption_m3_ = prev+curr;
+        target_water_consumption_m3_ = prev;
+    }
+
+    double Driver::totalWaterConsumption(Unit u)
+    {
+        assertQuantity(u, Quantity::Volume);
+        return convert(total_water_consumption_m3_, Unit::M3, u);
+    }
+
+    bool Driver::hasTotalWaterConsumption()
+    {
+        return true;
+    }
+
+    double Driver::targetWaterConsumption(Unit u)
+    {
+        return target_water_consumption_m3_;
+    }
+
+    bool Driver::hasTargetWaterConsumption()
+    {
+        return true;
+    }
+
+    string Driver::currentDate() {
+        return current_date_;
+    }
+
+    string Driver::previousDate() {
+        return previous_date_;
+    }
+}
+
+// Test: Duschen mkradio3 34333231 NOKEY
+// Comment: There is a problem in the decoding here, the data stored inside the telegram does not seem to properly encode/decode the year....
+// We should not report a current_date with a full year, if the year is actually not part of the telegram.
+// telegram=|2F446850313233347462A2_069F255900B029310000000306060906030609070606050509050505050407040605070500|
+// {"media":"warm water","meter":"mkradio3","name":"Duschen","id":"34333231","total_m3":13.8,"target_m3":8.9,"current_date":"2022-04-27T02:00:00Z","prev_date":"2018-12-31T02:00:00Z","timestamp":"1111-11-11T11:11:11Z"}
+// |Duschen;34333231;13.800000;8.900000;2022-04-27T02:00:00Z;2018-12-31T02:00:00Z;1111-11-11 11:11.11
--- src/meters.h
+++ src/meters.h
@@ -68,7 +68,6 @@ LIST_OF_METER_TYPES
     X(gransystems,T1_bit, ElectricityMeter, CCx01, CCx01) 		       \
     X(hydrocalm3, T1_bit, HeatMeter,        HYDROCALM3,  HydrocalM3)   \
     X(hydrodigit, T1_bit, WaterMeter,       HYDRODIGIT,  Hydrodigit)   \
-    X(mkradio3,   T1_bit, WaterMeter,       MKRADIO3,    MKRadio3)     \
     X(mkradio4,   T1_bit, WaterMeter,       MKRADIO4,    MKRadio4)     \
     X(multical302,C1_bit|T1_bit, HeatMeter,        MULTICAL302, Multical302)  \
     X(multical403,C1_bit, HeatMeter,        MULTICAL403, Multical403)  \
