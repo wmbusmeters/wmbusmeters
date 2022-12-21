a549754d657bab33671570f211c88514386af3f1
chpego <38792705+chpego@users.noreply.github.com>
2022-12-21 22:06:56 +0000

    switch echo to bashio::log.info

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -6,12 +6,12 @@ CONFIG_DATA_PATH=$(bashio::config 'data_path')
 CONFIG_CONF="$(jq --raw-output -c -M '.conf' $CONFIG_PATH)"
 CONFIG_METERS="$(jq --raw-output -c -M '.meters' $CONFIG_PATH)"
 
-echo "Syncing wmbusmeters configuration ..."
+bashio::log.info "Syncing wmbusmeters configuration ..."
 [ ! -d $CONFIG_DATA_PATH/logs/meter_readings ] && mkdir -p $CONFIG_DATA_PATH/logs/meter_readings
 [ ! -d $CONFIG_DATA_PATH/etc/wmbusmeters.d ] && mkdir -p $CONFIG_DATA_PATH/etc/wmbusmeters.d
 echo -e "$CONFIG_CONF" > $CONFIG_DATA_PATH/etc/wmbusmeters.conf
 
-echo "Registering meters ..."
+bashio::log.info "Registering meters ..."
 rm -f $CONFIG_DATA_PATH/etc/wmbusmeters.d/*
 meter_no=0
 IFS=$'\n'
@@ -19,12 +19,12 @@ for meter in $(jq -c -M '.meters[]' $CONFIG_PATH)
 do
     meter_no=$(( meter_no+1 ))
     METER_NAME=$(printf 'meter-%04d' "$(( meter_no ))")
-    echo "Adding $METER_NAME ..."
+    bashio::log.info "Adding $METER_NAME ..."
     METER_DATA=$(printf '%s\n' $meter | jq --raw-output -c -M '.') 
     echo -e "$METER_DATA" > $CONFIG_DATA_PATH/etc/wmbusmeters.d/$METER_NAME
 done
 
-echo "Generating MQTT configuration ... "
+bashio::log.info "Generating MQTT configuration ... "
 if bashio::config.exists "mqtt.host"
 then
   MQTT_HOST=$(bashio::config "mqtt.host")
@@ -38,7 +38,7 @@ else
   MQTT_PASSWORD=$(bashio::services mqtt "password")
 fi
 
-echo "Broker $MQTT_HOST will be used."
+bashio::log.info "Broker $MQTT_HOST will be used."
 pub_args=('-h' $MQTT_HOST )
 pub_args_quoted=('-h' \'$MQTT_HOST\' )
 [[ ! -z ${MQTT_PORT+x} ]] && pub_args+=( '-p' $MQTT_PORT ) && pub_args_quoted+=( '-p' \'$MQTT_PORT\' )
@@ -56,5 +56,5 @@ chmod a+x /wmbusmeters/mosquitto_pub.sh
 # Running MQTT discovery
 /mqtt_discovery.sh ${pub_args[@]} -c $CONFIG_PATH -w $CONFIG_DATA_PATH || true
 
-echo "Running wmbusmeters ..."
+bashio::log.info "Running wmbusmeters ..."
 /wmbusmeters/wmbusmeters --useconfig=$CONFIG_DATA_PATH
