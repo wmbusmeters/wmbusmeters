afcbd32193e3b779515f509621067fa9442941b0
Fredrik Öhrström <oehrstroem@gmail.com>
2024-06-19 21:22:22 +0200

    Andi252 fixed the weird mfct problem.

--- src/meters.cc
+++ src/meters.cc
@@ -112,8 +112,9 @@ bool DriverInfo::detect(uint16_t mfct, uchar type, uchar version)
     for (auto &dd : detect_)
     {
         if (dd.mfct == 0 && dd.type == 0 && dd.version == 0) continue; // Ignore drivers with no detection.
-        //some meters send mfct where the first character ist lower case, which results in mfct which are bigger
-        //than 32767, therefore restrict mfct to correct range
+        // Some weird meters (aptor08 and itronheat) send a mfct where the first character is lower case,
+        // which results in mfct which are bigger than 32767, therefore restrict mfct to correct range
+        // and the normal check will work.
         if (dd.mfct == (mfct & 0x7fff) && dd.type == type && dd.version == version) return true;
     }
     return false;
