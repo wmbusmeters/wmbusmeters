wmbusmeters/wmbusmeters

commit fccdea9345ee40450b93ccb58b4042efba61906a

Fix for meters with weird mfct value, like apator08 and itronheat.

src/meters.cc
@@ -112,7 +112,9 @@ bool DriverInfo::detect(uint16_t mfct, uchar type, uchar version)
     for (auto &dd : detect_)
     {
         if (dd.mfct == 0 && dd.type == 0 && dd.version == 0) continue; // Ignore drivers with no detection.
-        if (dd.mfct == mfct && dd.type == type && dd.version == version) return true;
+        //some meters send mfct where the first character ist lower case, which results in mfct which are bigger
+        //than 32767, therefore restrict mfct to correct range
+        if (dd.mfct == (mfct & 0x7fff) && dd.type == type && dd.version == version) return true;
     }
     return false;
 }

src/driver_apator08.cc
@@ -34,8 +34,8 @@ namespace
         di.setDefaultFields("name,id,total_m3,timestamp");
         di.setMeterType(MeterType::WaterMeter);
         di.addLinkMode(LinkMode::T1);
-        di.addDetection(0x8614/*APT?*/, 0x03,  0x03);
-        di.addDetection(0x8614/*APT?*/, 0x0F, 0x0F);
+        di.addDetection(MANUFACTURER_APT, 0x03,  0x03);
+        di.addDetection(MANUFACTURER_APT, 0x0F, 0x0F);
         di.usesProcessContent();
         di.setConstructor([](MeterInfo& mi, DriverInfo& di){ return shared_ptr<Meter>(new Driver(mi, di)); });
     });

commit 92fcc4d06fca2d21a8b5b6c6194246493b0a31ce

Fix driver autodetection regression on mfct name matching
fccdea9345ee40450b93ccb58b4042efba61906a introduced a change that
was supposed to fix mfct matching on "weird" metters that reported
fist letter as lowercase. It however broke autodetection of
manufacturers that report it correctly...

src/meters.cc
@@ -115,7 +115,7 @@ bool DriverInfo::detect(uint16_t mfct, uchar type, uchar version)
         // Some weird meters (aptor08 and itronheat) send a mfct where the first character is lower case,
         // which results in mfct which are bigger than 32767, therefore restrict mfct to correct range
         // and the normal check will work.
-        if (dd.mfct == (mfct & 0x7fff) && dd.type == type && dd.version == version) return true;
+        if ((dd.mfct & 0x7fff) == (mfct & 0x7fff) && dd.type == type && dd.version == version) return true;
     }
     return false;
 }