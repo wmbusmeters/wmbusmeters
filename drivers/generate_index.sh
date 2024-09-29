9f1d45fc1f281546afa836f9d1ea1f686662df04
Fredrik Öhrström <oehrstroem@gmail.com>
2024-09-29 10:08:08 +0200

    Add generation of xmq drivers as web pages.

--- drivers/generate_index.sh
+++ drivers/generate_index.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+mkdir -p ../build/web
+OUT=../build/web/index.htmq
+
+for i in src/*
+do
+    xmq $i render-html --theme=darkbg > ../build/web/$(basename $i).html
+done
+
+echo "!DOCTYPE=html" > $OUT
+echo "html {" >> $OUT
+echo "body {" >> $OUT
+
+for i in $(ls ../build/web/*xmq.html | sort)
+do
+    HREF=$(basename $i)
+    NAME=${HREF%%.*}
+    echo "a(href=${HREF})=${NAME}.xmq" >> $OUT
+    echo br  >> $OUT
+done
+
+echo "}}" >> $OUT
+
+xmq $OUT to-html > ../build/web/index.html
