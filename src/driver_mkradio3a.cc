100a19a9cd1600599af50e9be30371883d2b9bd5
salaun29 <44942939+salaun29@users.noreply.github.com>
2024-11-28 10:40:05 +0100

    Create driver_mkradio3a.cc
    Auto driver    : mkradio3a
    Similar driver : unknown 00/00
    Using driver   : mkradio3a 00/00
    000   : 36 length (54 bytes)
    001   : 44 dll-c (from meter SND_NR)
    002   : 6850 dll-mfct (TCH)
    004   : 42065662 dll-id (62560642)
    008   : 50 dll-version
    009   : 72 dll-type (Cold water)
    010   : a2 tpl-ci-field (Mfct specific)
    011 C?: 0C007C3110250000293400373A002E38000E15002F37003A39003835002F24003930001D2500312500162900 mfct specific
    
    {
        "_":"telegram",
        "media":"cold water",
        "meter":"mkradio3a",
        "name":"",
        "id":"62560642",
        "target_m3":1335.3,
        "total_m3":948.8,
        "current_date":"2024-11-28T02:00:00Z",
        "prev_date":"2024-11-28T02:00:00Z",
        "timestamp":"2024-11-28T09:39:52Z"
    }

--- src/driver_mkradio3a.cc
+++ src/driver_mkradio3a.cc
@@ -78,16 +78,15 @@ namespace
         setStringValue("current_date", tostrprintf("%d-%02d-%02dT02:00:00Z", prev_date_year+2000, prev_date_month, prev_date_day));
 
         // Previous consumption
-        uchar prev_lo = content[4];
-        uchar prev_me = content[5];
-        uchar prev_hi = content[6];
-        double prev = (256.0*256.0*prev_hi+256.0*prev_me+prev_lo)/10.0;
+        uchar prev_lo = content[8];
+        uchar prev_hi = content[9];
+        double prev = (256.0*prev_hi+prev_lo)/10.0;
 
         // Current consumption
-        // uchar curr_lo = content[7];
-        // uchar curr_hi = content[8];
-        // double curr = (256.0*curr_hi+curr_lo)/10.0;
-        double curr = prev;
+        uchar curr_lo = content[4];
+        uchar curr_me = content[5];
+        uchar curr_hi = content[6];
+        double curr = (256.0*256.0*curr_hi+256.0*curr_me+curr_lo)/10.0;
 
         double total_water_consumption_m3 = curr;
         setNumericValue("total", Unit::M3, total_water_consumption_m3);
@@ -101,5 +100,5 @@ namespace
 // 102500 => 002510 : water meter value (same as digital one)
 // 2934 => 3429 : ???
 // telegram=|36446850420656625072A2_0C007C3110250000293400373A002E38000E15002F37003A39003835002F24003930001D2500312500162900|
-// {"_":"telegram", "media":"cold water", "meter":"mkradio3a", "name":"", "id":"62560642", "target_m3":948.8, "total_m3":948.8, "current_date":"2024-11-28T02:00:00Z", "prev_date":"2024-11-28T02:00:00Z", "timestamp":"2024-11-28T08:18:58Z"}
-// 
+// { "_":"telegram", "media":"cold water", "meter":"mkradio3a", "name":"", "id":"62560642", "target_m3":1335.3, "total_m3":948.8, "current_date":"2024-11-28T02:00:00Z", "prev_date":"2024-11-28T02:00:00Z", "timestamp":"2024-11-28T09:36:01Z" } 
+//
