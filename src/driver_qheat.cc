3a561a13f8ed2d2b19a3bbdac24893b706cd4d6e
matix2120 <mateusz@stojek.eu>
2025-05-18 14:01:28 +0200

    Update driver_qheat.cc - Add new device ID

--- src/driver_qheat.cc
+++ src/driver_qheat.cc
@@ -37,6 +37,7 @@ namespace
         di.addDetection(MANUFACTURER_QDS, 0x04,  0x23);
         di.addDetection(MANUFACTURER_QDS, 0x04,  0x46);
         di.addDetection(MANUFACTURER_QDS, 0x37,  0x23);
+        di.addDetection(MANUFACTURER_QDS, 0x37,  0x47);
         di.usesProcessContent();
         di.setConstructor([](MeterInfo& mi, DriverInfo& di){ return shared_ptr<Meter>(new Driver(mi, di)); });
     });
