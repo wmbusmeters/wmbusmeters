4b6a328579d5c9fefb6908f0ea58f95be476d706
Fredrik Öhrström <oehrstroem@gmail.com>
2025-12-08 00:35:01 +0100

    Avoid always loading all drivers.

--- src/main.cc
+++ src/main.cc
@@ -89,7 +89,6 @@ int main(int argc, char **argv)
 
     enableEarlyLoggingFromCommandLine(argc, argv);
     prepareBuiltinDrivers();
-    loadAllBuiltinDrivers();
 
     auto config = parseCommandLine(argc, argv);
 
