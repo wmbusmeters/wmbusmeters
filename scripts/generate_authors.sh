f3352c4ccbf636367fcfd5186aa93a85106d20de
Aras Abbasi <aras.abbasi@googlemail.com>
2026-03-21 10:47:40 +0100

    chore: improve generate_authors.sh
    generate_authors.sh doesnt need now the target file defined and echoes to stdoud to see the generated output. Also afterwards the temp folder will be deleted. Also using the current year to set the right year for the top line.

--- scripts/generate_authors.sh
+++ scripts/generate_authors.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
-# Copyright (C) 2022 Fredrik Öhrström (gpl-3.0-or-later)
+# Copyright (C) 2026 Fredrik Öhrström (gpl-3.0-or-later)
 
 TMP=$(mktemp)
-OUT=$1
 
-(grep -rEc "Copyright \(C\) (....-)?.... [^\(]+ \(.+\)" src drivers/src | grep :0 > $TMP)
+# Use stdout if no argument is given, otherwise write to the specified file
+if [ -z "$1" ]; then
+    OUT="/dev/stdout"
+else
+    OUT="$1"
+fi
 
-if [ -s $TMP ]
+# Retrieve the current year to update the copyright notice
+CURRENT_YEAR=$(date +%Y)
+
+(grep -rEc "Copyright \(C\) (....-)?.... [^\(]+ \(.+\)" src drivers/src | grep :0 > "$TMP")
+
+if [ -s "$TMP" ]
 then
     echo "These files do not have a proper copyright notice:"
-    cat $TMP | sed 's/:0//'
+    sed 's/:0//' "$TMP"
     exit 1
 fi
 
-(grep -rEo "Copyright \(C\) (....-)?.... [^\(]+ \(.+\)" src drivers/src | cut -f 2 -d ':' | tr -s ' ' | sed 's/(C) \([0-9][0-9][0-9][0-9]\) /(C) \1-\1 /' >  $TMP)
+(grep -rEo "Copyright \(C\) (....-)?.... [^\(]+ \(.+\)" src drivers/src \
+    | cut -f 2 -d ':' \
+    | tr -s ' ' \
+    | sed 's/(C) \([0-9][0-9][0-9][0-9]\) /(C) \1-\1 /' > "$TMP")
 
-echo 'R"AUTHORS(' > $OUT
+{
+    echo 'R"AUTHORS('
+    echo "Copyright (C) 2017-${CURRENT_YEAR} Fredrik Öhrström"
 
-echo "Copyright (C) 2017-2022 Fredrik Öhrström" >> $OUT
+    sed 's/ <.*>//' "$TMP" | \
+        sed 's/ [0-9][0-9][0-9][0-9]-/ /' | \
+        sed 's/ (.[^)].*)//g' | \
+        grep -v Öhrström | \
+        sort -rn | \
+        sed 's/Copyright (C) /                   /' | \
+        uniq
 
-cat $TMP | sed 's/ <.*>//' | \
-    sed 's/ [0-9][0-9][0-9][0-9]-/ /' | \
-    sed 's/ (.[^)].*)//g' | \
-    grep -v Öhrström | \
-    sort -rn  | \
-    sed 's/Copyright (C) /                   /' | \
-    uniq >> $OUT
+    echo ')AUTHORS";'
+} > "$OUT"
 
-echo ')AUTHORS";' >> $OUT
+# Cleanup the temporary file
+rm -f "$TMP"
