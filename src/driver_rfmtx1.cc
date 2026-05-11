5794d259eddd883656b9f78e92f08fd563b7b32c
Aras Abbasi <aras.abbasi@googlemail.com>
2026-05-11 17:03:51 +0200

    chore: simplify rfmtx1

--- src/driver_rfmtx1.cc
+++ src/driver_rfmtx1.cc
@@ -60,12 +60,24 @@ namespace
             .set(VIFRange::DateTime));
     }
 
-    uchar decode_vectors_[16][6] = { { 117, 150, 122, 16, 26, 10 }, { 91, 127, 112, 19, 34, 19 }, { 179, 24, 185, 11, 142, 153 },
-                                     { 142, 125, 121, 7, 74, 22 }, { 181, 145, 7, 154, 203, 105 }, { 184, 163, 50, 161, 57, 14 },
-                                     { 189, 128, 156, 126, 96, 153 }, { 39, 92, 180, 196, 128, 163 }, { 48, 208, 10, 206, 25, 3 },
-                                     { 194, 76, 240, 5, 165, 134 }, { 84, 75, 22, 152, 17, 94 }, { 75, 238, 12, 201, 125, 162 },
-                                     { 135, 202, 74, 72, 228, 31 }, { 196, 135, 119, 46, 138, 232 }, { 227, 48, 189, 120, 87, 140 },
-                                     { 164, 154, 57, 111, 40, 5 } };
+    uchar decode_vectors_[64] = {
+        122, 16, 26, 10,
+        112, 19, 34, 19,
+        185, 11, 142, 153,
+        121, 7, 74, 22,
+        7, 154, 203, 105,
+        50, 161, 57, 14,
+        156, 126, 96, 153,
+        180, 196, 128, 163,
+        10, 206, 25, 3,
+        240, 5, 165, 134,
+        22, 152, 17, 94,
+        12, 201, 125, 162,
+        74, 72, 228, 31,
+        119, 46, 138, 232,
+        189, 120, 87, 140,
+        57, 111, 40, 5
+    };
 
     void Driver::processContent(Telegram *t)
     {
@@ -77,21 +89,20 @@ namespace
 
             debugPayload("(rftx1) decoding raw frame", frame);
 
-            uchar decoded_total[6];
+            int key = frame[11];
+            int row = key & 0x0F;
+            int base = row * 4;
 
-            for (int i = 0; i < 6; ++i)
-            {
-                decoded_total[i] = (uchar)(frame[0xf + i] ^ frame[0xb] ^ decode_vectors_[frame[0xb] & 0x0f][i]);
+            double total = 0.0;
+            double mul = 1.0;
+
+            for (int i = 2; i < 6; ++i) {
+                uchar decoded = frame[15 + i] ^ key ^ decode_vectors_[base + (i - 2)];
+                total += mul * bcd2bin(decoded);
+                mul *= 100.0;
             }
 
-            double total = 0;
-            double mul = 1;
-            for (int i = 2; i < 6; ++i)
-            {
-                total += mul*bcd2bin(decoded_total[i]);
-                mul *= 100;
-            }
-            setNumericValue("total", Unit::M3, total/1000.0);
+            setNumericValue("total", Unit::M3, total / 1000.0);
 
             int o = 28; // Offset to datetime.
             int y = 2000+bcd2bin(frame[o+5]);
