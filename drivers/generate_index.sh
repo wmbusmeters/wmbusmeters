84d9ea2fa42cf0e022af48572461c191b83161fb
Fredrik Öhrström <oehrstroem@gmail.com>
2026-09-19 22:03:47 +0200

    Fix ebeco500 driver.

--- drivers/generate_index.sh
+++ drivers/generate_index.sh
@@ -12,7 +12,7 @@ cp src/*.xmq ../build/web
 echo -n "Generating html for xmq files"
 for i in src/*
 do
-    XMQ_BG=light XMQ_THEME="BG=#ffffff" xmq $i render-html > ../build/web/$(basename $i).html
+    XMQ_BG=dark xmq $i render-html > ../build/web/$(basename $i).html
     echo -n "."
 done
 echo "done."
