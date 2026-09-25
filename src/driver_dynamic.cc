d0d72d4752fb0e09709ba8ee1449ac13ae8e0c61
Fredrik Öhrström <oehrstroem@gmail.com>
2026-09-25 15:40:24 +0200

    Test auto driver vs explicit driver in driver tests. They should be the same.

--- src/driver_dynamic.cc
+++ src/driver_dynamic.cc
@@ -35,7 +35,7 @@ string check_driver_name(const char *name, string file);
 string check_aliases(const char *aliases, string file);
 MeterType check_meter_type(const char *meter_type_s, string file);
 string check_default_fields(const char *fields, string file);
-void check_detection_triplets(DriverInfo *di, string file);
+void check_detection_triplets(DriverInfo *di, string file, const char *deprecated_by);
 
 string check_field_name(const char *name, DriverDynamic *dd);
 string check_field_ixml(const char *ixml, DriverDynamic *dd);
@@ -120,6 +120,8 @@ bool DriverDynamic::load(DriverInfo *di, const string &file_name, const char *co
         string name = check_driver_name(xmqGetString(doc, "/driver/name"), file);
         di->setName(name);
 
+        const char *deprecated_by = xmqGetString(doc, "/driver/deprecated_by");
+
         string aliases = check_aliases(xmqGetString(doc, "/driver/aliases"), file);
         di->setAliases(aliases);
 
@@ -144,7 +146,7 @@ bool DriverDynamic::load(DriverInfo *di, const string &file_name, const char *co
         xmqForeach(doc, "/driver/mfct_tpl_status_bits", (XMQNodeCallback)add_mfct_tpl_status, di);
         xmqForeach(doc, "/driver/default_keys/key", (XMQNodeCallback)add_default_key, di);
 
-        check_detection_triplets(di, file);
+        check_detection_triplets(di, file, deprecated_by);
 
         di->setConstructor([](MeterInfo& mi, DriverInfo& di){ return shared_ptr<Meter>(new DriverDynamic(mi, di)); });
 
@@ -870,9 +872,9 @@ string check_default_fields(const char *default_fields, string file)
     return default_fields;
 }
 
-void check_detection_triplets(DriverInfo *di, string file)
+void check_detection_triplets(DriverInfo *di, string file, const char *deprecated_by)
 {
-    if (di->mvts().size() == 0)
+    if (di->mvts().size() == 0 && deprecated_by == NULL)
     {
         warning("(driver) error in %s, cannot find any detection triplets: driver/detect/mvt\n"
                 "%s\n"
